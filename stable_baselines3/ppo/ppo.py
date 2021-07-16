5179e946986255a829f7bd927a9785535aaec83a
Bruno Brito <bruno.debrito@tudelft.nl>
2021-07-16 14:11:17 +0200

    no idea

--- stable_baselines3/ppo/ppo.py
+++ stable_baselines3/ppo/ppo.py
@@ -1,16 +1,57 @@
-import warnings
-from typing import Any, Dict, Optional, Type, Union
+from typing import Any, Callable, Dict, Optional, Type, Union
 
 import numpy as np
 import torch as th
 from gym import spaces
 from torch.nn import functional as F
 
+from stable_baselines3.common import logger
 from stable_baselines3.common.on_policy_algorithm import OnPolicyAlgorithm
 from stable_baselines3.common.policies import ActorCriticPolicy
-from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback, Schedule
+from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback
 from stable_baselines3.common.utils import explained_variance, get_schedule_fn
 
+import itertools
+import torch.utils.data as th_data
+from imitation.data import buffer, types, wrappers
+from typing import (
+    Callable,
+    Iterable,
+    Iterator,
+    Mapping,
+    Optional,
+    Sequence,
+    Type,
+    TypeVar,
+    Union,
+)
+
+T = TypeVar("T")
+
+
+def endless_iter(iterable: Iterable[T]) -> Iterator[T]:
+    """Generator that endlessly yields elements from iterable.
+
+    If any call to `iter(iterable)` has no elements, then this function raises
+    ValueError.
+
+    >>> x = range(2)
+    >>> it = endless_iter(x)
+    >>> next(it)
+    0
+    >>> next(it)
+    1
+    >>> next(it)
+    0
+
+    """
+    try:
+        next(iter(iterable))
+    except StopIteration:
+        err = ValueError(f"iterable {iterable} had no elements to iterate over.")
+        raise err
+
+    return itertools.chain.from_iterable(itertools.repeat(iterable))
 
 class PPO(OnPolicyAlgorithm):
     """
@@ -28,9 +69,7 @@ class PPO(OnPolicyAlgorithm):
     :param learning_rate: The learning rate, it can be a function
         of the current progress remaining (from 1 to 0)
     :param n_steps: The number of steps to run for each environment per update
-        (i.e. rollout buffer size is n_steps * n_envs where n_envs is number of environment copies running in parallel)
-        NOTE: n_steps * n_envs must be greater than 1 (because of the advantage normalization)
-        See https://github.com/pytorch/pytorch/issues/29372
+        (i.e. batch size is n_steps * n_env where n_env is number of environment copies running in parallel)
     :param batch_size: Minibatch size
     :param n_epochs: Number of epoch when optimizing the surrogate loss
     :param gamma: Discount factor
@@ -66,16 +105,17 @@ class PPO(OnPolicyAlgorithm):
 
     def __init__(
         self,
+        expert_data,
         policy: Union[str, Type[ActorCriticPolicy]],
         env: Union[GymEnv, str],
-        learning_rate: Union[float, Schedule] = 3e-4,
+        learning_rate: Union[float, Callable] = 3e-4,
         n_steps: int = 2048,
-        batch_size: Optional[int] = 64,
+        batch_size: Optional[int] = 256,
         n_epochs: int = 10,
         gamma: float = 0.99,
         gae_lambda: float = 0.95,
-        clip_range: Union[float, Schedule] = 0.2,
-        clip_range_vf: Union[None, float, Schedule] = None,
+        clip_range: float = 0.05,
+        clip_range_vf: Optional[float] = None,
         ent_coef: float = 0.0,
         vf_coef: float = 0.5,
         max_grad_norm: float = 0.5,
@@ -110,46 +150,41 @@ class PPO(OnPolicyAlgorithm):
             create_eval_env=create_eval_env,
             seed=seed,
             _init_setup_model=False,
-            supported_action_spaces=(
-                spaces.Box,
-                spaces.Discrete,
-                spaces.MultiDiscrete,
-                spaces.MultiBinary,
-            ),
         )
 
-        # Sanity check, otherwise it will lead to noisy gradient and NaN
-        # because of the advantage normalization
-        assert (
-            batch_size > 1
-        ), "`batch_size` must be greater than 1. See https://github.com/DLR-RM/stable-baselines3/issues/440"
-
-        if self.env is not None:
-            # Check that `n_steps * n_envs > 1` to avoid NaN
-            # when doing advantage normalization
-            buffer_size = self.env.num_envs * self.n_steps
-            assert (
-                buffer_size > 1
-            ), f"`n_steps * n_envs` must be greater than 1. Currently n_steps={self.n_steps} and n_envs={self.env.num_envs}"
-            # Check that the rollout buffer size is a multiple of the mini-batch size
-            untruncated_batches = buffer_size // batch_size
-            if buffer_size % batch_size > 0:
-                warnings.warn(
-                    f"You have specified a mini-batch size of {batch_size},"
-                    f" but because the `RolloutBuffer` is of size `n_steps * n_envs = {buffer_size}`,"
-                    f" after every {untruncated_batches} untruncated mini-batches,"
-                    f" there will be a truncated mini-batch of size {buffer_size % batch_size}\n"
-                    f"We recommend using a `batch_size` that is a factor of `n_steps * n_envs`.\n"
-                    f"Info: (n_steps={self.n_steps} and n_envs={self.env.num_envs})"
-                )
         self.batch_size = batch_size
         self.n_epochs = n_epochs
         self.clip_range = clip_range
         self.clip_range_vf = clip_range_vf
         self.target_kl = target_kl
+        self.gama = 1.0
+        self.decay = 0.99
+
+        if expert_data is not None:
+            if isinstance(expert_data, types.Transitions):
+                if len(expert_data) < batch_size:
+                    raise ValueError(
+                        "Provided Transitions instance as `expert_data` argument but "
+                        "len(expert_data) < expert_batch_size. "
+                        f"({len(expert_data)} < {batch_size})."
+                    )
+
+                self.expert_data_loader = th_data.DataLoader(
+                    expert_data,
+                    batch_size=batch_size,
+                    collate_fn=types.transitions_collate_fn,
+                    shuffle=True,
+                    drop_last=True,
+                )
+            else:
+                self.expert_data_loader = expert_data
+            self._endless_expert_iterator = endless_iter(self.expert_data_loader)
 
         if _init_setup_model:
             self._setup_model()
+
+    def _next_expert_batch(self) -> Mapping:
+        return next(self._endless_expert_iterator)
 
     def _setup_model(self) -> None:
         super(PPO, self)._setup_model()
@@ -164,7 +199,8 @@ class PPO(OnPolicyAlgorithm):
 
     def train(self) -> None:
         """
-        Update policy using the currently gathered rollout buffer.
+        Update policy using the currently gathered
+        rollout buffer.
         """
         # Update optimizer learning rate
         self._update_learning_rate(self.policy.optimizer)
@@ -174,13 +210,12 @@ class PPO(OnPolicyAlgorithm):
         if self.clip_range_vf is not None:
             clip_range_vf = self.clip_range_vf(self._current_progress_remaining)
 
-        entropy_losses = []
+        entropy_losses, all_kl_divs = [], []
         pg_losses, value_losses = [], []
+        mse = []
         clip_fractions = []
 
-        continue_training = True
-
-        # train for n_epochs epochs
+        # train for gradient_steps epochs
         for epoch in range(self.n_epochs):
             approx_kl_divs = []
             # Do a complete pass on the rollout buffer
@@ -239,20 +274,19 @@ class PPO(OnPolicyAlgorithm):
 
                 loss = policy_loss + self.ent_coef * entropy_loss + self.vf_coef * value_loss
 
-                # Calculate approximate form of reverse KL Divergence for early stopping
-                # see issue #417: https://github.com/DLR-RM/stable-baselines3/issues/417
-                # and discussion in PR #419: https://github.com/DLR-RM/stable-baselines3/pull/419
-                # and Schulman blog: http://joschu.net/blog/kl-approx.html
+                # Get expert batch
+                expert_samples = self._next_expert_batch()
                 with th.no_grad():
-                    log_ratio = log_prob - rollout_data.old_log_prob
-                    approx_kl_div = th.mean((th.exp(log_ratio) - 1) - log_ratio).cpu().numpy()
-                    approx_kl_divs.append(approx_kl_div)
+                    # Compute value for the last timestep
+                    obs_tensor = th.as_tensor(expert_samples['obs']).to(self.device)
+                    actions_tensor = th.as_tensor(expert_samples['acts']).to(self.device)
 
-                if self.target_kl is not None and approx_kl_div > 1.5 * self.target_kl:
-                    continue_training = False
-                    if self.verbose >= 1:
-                        print(f"Early stopping at step {epoch} due to reaching max kl: {approx_kl_div:.2f}")
-                    break
+                # BC-GAIL
+                _, alogprobs, _ = self.policy.evaluate_actions(obs_tensor, actions_tensor)
+                bcloss = -alogprobs.mean()
+                # action loss is weighted sum
+                loss = self.gama * bcloss + (1 - self.gama) * loss
+                # Multiply this coeff with decay factor
 
                 # Optimization step
                 self.policy.optimizer.zero_grad()
@@ -260,28 +294,34 @@ class PPO(OnPolicyAlgorithm):
                 # Clip grad norm
                 th.nn.utils.clip_grad_norm_(self.policy.parameters(), self.max_grad_norm)
                 self.policy.optimizer.step()
+                approx_kl_divs.append(th.mean(rollout_data.old_log_prob - log_prob).detach().cpu().numpy())
 
-            if not continue_training:
+            all_kl_divs.append(np.mean(approx_kl_divs))
+
+            if self.target_kl is not None and np.mean(approx_kl_divs) > 1.5 * self.target_kl:
+                print(f"Early stopping at step {epoch} due to reaching max kl: {np.mean(approx_kl_divs):.2f}")
                 break
 
         self._n_updates += self.n_epochs
-        explained_var = explained_variance(self.rollout_buffer.values.flatten(), self.rollout_buffer.returns.flatten())
+        self.gama *= self.decay
+        explained_var = explained_variance(self.rollout_buffer.returns.flatten(), self.rollout_buffer.values.flatten())
 
         # Logs
-        self.logger.record("train/entropy_loss", np.mean(entropy_losses))
-        self.logger.record("train/policy_gradient_loss", np.mean(pg_losses))
-        self.logger.record("train/value_loss", np.mean(value_losses))
-        self.logger.record("train/approx_kl", np.mean(approx_kl_divs))
-        self.logger.record("train/clip_fraction", np.mean(clip_fractions))
-        self.logger.record("train/loss", loss.item())
-        self.logger.record("train/explained_variance", explained_var)
+        logger.record("train/entropy_loss", np.mean(entropy_losses))
+        logger.record("train/policy_gradient_loss", np.mean(pg_losses))
+        logger.record("train/value_loss", np.mean(value_losses))
+        logger.record("train/approx_kl", np.mean(approx_kl_divs))
+        logger.record("train/clip_fraction", np.mean(clip_fractions))
+        logger.record("train/loss", loss.item())
+        logger.record("train/explained_variance", explained_var)
+        logger.record("train/gamma", self.gama)
         if hasattr(self.policy, "log_std"):
-            self.logger.record("train/std", th.exp(self.policy.log_std).mean().item())
+            logger.record("train/std", th.exp(self.policy.log_std).mean().item())
 
-        self.logger.record("train/n_updates", self._n_updates, exclude="tensorboard")
-        self.logger.record("train/clip_range", clip_range)
+        logger.record("train/n_updates", self._n_updates, exclude="tensorboard")
+        logger.record("train/clip_range", clip_range)
         if self.clip_range_vf is not None:
-            self.logger.record("train/clip_range_vf", clip_range_vf)
+            logger.record("train/clip_range_vf", clip_range_vf)
 
     def learn(
         self,
